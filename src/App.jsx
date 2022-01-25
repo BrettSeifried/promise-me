447dcd70eedc40bce4e9408b6759b85a17967005
Brett Seifried <seifriedbrett@gmail.com>
2022-01-25 11:15:42 -0800

    .then and await/asycn working

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import { useEffect } from 'react';
+import { animeData, animeDataThen } from './services/promise-me';
+
 export default function App() {
+  useEffect(() => {
+    animeData();
+    animeDataThen();
+  });
+  // const awaitTest = animeData();
+  // console.log('await', awaitTest);
+  // const thenTest = animeDataThen();
+  // console.log('then', thenTest);
   return <h1>Hello World</h1>;
 }
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -1,6 +1,27 @@
+export async function animeData() {
+  const resp = await fetch('https://api.jikan.moe/v4/anime?q=kuroko&sfw');
+  const data = await resp.json();
+  console.log('await', data);
+  return data;
+}
+
+export const animeDataThen = () => {
+  const data = fetch('https://api.jikan.moe/v4/anime?q=kuroko&sfw')
+    .then((data) => data.json())
+    .then((response) => {
+      console.log('then', response);
+    });
+  return response;
+};
+
 /**
- * TODO:
+ * TODO: write a fetch request to an API url using .then()
+ * THEN: writ ea fetch request using async/await
+ *
+ * MAKE SURE they ar eidentical
  */
+// console.log(.then)
+// constole.log(async/await)
 
 /**
  * TODO:
